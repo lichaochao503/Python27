7e951f19c2c3943bf157ef7e8527dad1b0f423fd
lichaochao503 <503990154@qq.com>
2017-07-04 19:08:06 +0800

    20170704

--- src/Test/20170614.py
+++ src/Test/20170614.py
@@ -8,3 +8,4 @@ Created on Jun 14, 2017
 if __name__ == '__main__':
     pass
 print '你好啊'
+print "你好，世界";
---
+++ src/Test/20170704py2.py
@@ -0,0 +1,43 @@
+# encoding: utf-8
+'''
+Created on Jul 4, 2017
+
+@author: byhx_lcc
+'''
+print "Start learn python";
+
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
+
+print days[1];
+
+word = 'word'
+sentence = "这是一个句子。"
+paragraph = """这是一个段落。
+包含了多个语句"""
+print word; # 这是一个注释
+'''
+test = 123
+'''
+
+# raw_input("\n\nPress the enter key to exit.")
+
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+x = 'a'
+y = 'b'
+# 换行输出 不换行输出
+print "a"; print x,; print y;
+
+counter = 100 # 整形
+miles = 1000.0 # 浮点型
+name = "Jhon" # 字符串
+
+print counter
+print miles
+print name
+
+a = b = c = 1;
+# a, b, c = 1, 2, "john"
+print a,; print b; print c
+
